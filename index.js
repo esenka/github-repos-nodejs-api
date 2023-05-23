ab010910056827bdbda0f931fc61259e45803871
Esen Küçüktütüncü <esenktutuncu@gmail.com>
2023-05-23 17:01:20 +0200

    making small changes

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 8081;
 
-app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
-  try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
+import { Client } from 'node-osc';
 
-    res.send(repos);
-  } catch (error) {
-    res.status(400).send('Error while getting list of repositories');
+'use strict';
+const { Client, Message } = require('node-osc');
+
+const client = new Client('127.0.0.1', 3333);
+
+const message = new Message('/address');
+message.append('testing');
+message.append('testing');
+message.append(123);
+
+client.send(message, (err) => {
+  if (err) {
+    console.error(new Error(err));
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+  client.close();
 });
